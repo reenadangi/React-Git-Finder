6eac69727489636570bd6024991d57a433d5f184
reenadangi <reena.dangi@gmail.com>
2020-05-17 12:37:21 -0500

    React-GitFinder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
-
-function App() {
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import Search from './components/users/Search'
+import Axios from 'axios';
+class App extends Component {
+  state={
+    users:[],
+    loading:false
+  }
+  async componentDidMount(){
+    this.setState({loading:true});
+    const res=await Axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({
+      users:res.data,
+      loading:false});
+    console.log(this.state.users)
+  }
+  searchUsers=async text=>{
+    console.log("In search"+text)
+    const res=await Axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    console.log(res)
+    this.setState({
+      users:res.data.items,
+      loading:false});
+    console.log(this.state.users)
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   <div className='App'>
+     <Navbar />
+     <div className="container">
+     <Search searchUsers={this.searchUsers}/> 
+     <Users loading={this.state.loading} users={this.state.users}/>
 
+     </div>
+     
+   </div>
+
+  );
+      
+  }
+}
 export default App;
